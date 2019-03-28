6924c017f0e7d44d7cd3a90c53e54e5995c8d76f
Jaya Chandra <jayac9700@gmail.com>
2019-03-28 15:51:58 +0530

    data store service all methods added

--- src/app/@core/datastore.service.ts
+++ src/app/@core/datastore.service.ts
@@ -3,12 +3,29 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class DatastoreService {
-  private currentStoreSubject = new BehaviorSubject<DynamicObject>({} as DynamicObject);
+  private currentStoreSubject = new BehaviorSubject<DynamicObject>(
+    {} as DynamicObject
+  );
   public currentStore = this.currentStoreSubject.asObservable();
   constructor() {}
   setData(key: string, value: any) {
-    const currentStore = Object.assign({} as DynamicObject);
+    const currentStore = this.getCurrentStore();
     currentStore[key] = value;
+    this.currentStoreSubject.next(currentStore);
+  }
+  getCurrentStore(): DynamicObject {
+    console.log(this.currentStoreSubject.value);
+    return this.currentStoreSubject.value;
+  }
+  getData(key: string): any {
+    const currentStore = this.getCurrentStore();
+    return currentStore[key];
+  }
+  clearStore() {
+    const currentStore = this.getCurrentStore();
+    Object.keys(currentStore).forEach(key => {
+      delete currentStore[key];
+    });
     this.currentStoreSubject.next(currentStore);
   }
 }
